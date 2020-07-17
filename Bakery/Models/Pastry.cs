2dc5fff8be9b9d1bf5b15e0bdc3f4e854c3ab821
Megan Hepner <megan.hepner@gmail.com>
2020-07-17 10:10:57 -0700

    run passing test TallyPastryOrder_Charges1DollarsForEveryThirdPastry_True()

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -11,7 +11,14 @@ namespace Bakery
       int currentOrder = 0;
       for (int i = 1; i <= pastryOrder; i++)
       {
+        if (i%3 == 0)
+        {
+          currentOrder += 1;
+        }
+        else
+        {
         currentOrder += 2;
+        }
       }
       return currentOrder;
     }
