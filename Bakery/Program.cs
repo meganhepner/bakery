ae865696c1a4615c0cbcee902b83c33ad92703a9
Megan Hepner <megan.hepner@gmail.com>
2020-07-17 16:00:56 -0700

    finalize working version without using list

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -16,34 +16,17 @@ namespace Bakery.Models
       {
         Console.WriteLine("**Sale!** Sourdough Boules $5 or Buy 2 Get 1 Free");
         Console.WriteLine("**Sale!** Sugar Cookies $2 or 3 for $5");
-        Console.WriteLine("Would you like to purchase any bread today? Press ['Y'] for Yes, or ['Enter'] to continue to other Bakery options");
-        string bread = Console.ReadLine();
-        if (bread == "Y" || bread == "y")
-        {
-          Console.WriteLine("How many loaves would you like to purchase?");
-          string userBreadInput = Console.ReadLine();
-          int breadOrder = int.Parse(userBreadInput);
-          int breadTotal = newBread.TallyBreadOrder(breadOrder);
-          Console.WriteLine("Your order total is $" + breadTotal);
-        }
-        else
-        {
-          Console.WriteLine("Would you like to purchase any pastries today? Press ['Y'] for Yes, or ['Enter'] to see your shopping cart"); 
-          string pastry = Console.ReadLine();
-          if (pastry == "Y" || pastry == "y")
-          {
-            Console.WriteLine("How many pastries would you like to purchase?");
-            string userPastryInput = Console.ReadLine();
-            int pastryOrder = int.Parse(userPastryInput);
-            int pastryTotal = newPastry.TallyPastryOrder(pastryOrder);
-            Console.WriteLine("Your order total is $" + pastryTotal);
-          }
-          else
-          {
-            Console.Write("Your order total is $" + (newBread.TotalBreadOrder() + newPastry.TotalPastryOrder()));
-          }
-        }
-
+        Console.WriteLine("How many loaves would you like to purchase? If none, please enter 0!");
+        string userBreadInput = Console.ReadLine();
+        int breadOrder = int.Parse(userBreadInput);
+        int breadTotal = newBread.TallyBreadOrder(breadOrder);
+        Console.WriteLine("That will be $" + breadTotal);
+        Console.WriteLine("How many pastries would you like to purchase? If none, please enter 0!");
+        string userPastryInput = Console.ReadLine();
+        int pastryOrder = int.Parse(userPastryInput);
+        int pastryTotal = newPastry.TallyPastryOrder(pastryOrder);
+        Console.WriteLine("That will be $" + pastryTotal);
+        Console.Write("Your order total is $" + (breadTotal + pastryTotal));
       }
       else 
       {
