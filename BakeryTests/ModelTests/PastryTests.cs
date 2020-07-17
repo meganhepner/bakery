f698d367c0cfdf052a3e55c65e8f9936155a6bca
Megan Hepner <megan.hepner@gmail.com>
2020-07-17 10:08:28 -0700

    create failing test for TallyPastryOrder_Charges1DollarsForEveryThirdPastry_True()

--- BakeryTests/ModelTests/PastryTests.cs
+++ BakeryTests/ModelTests/PastryTests.cs
@@ -6,11 +6,17 @@ namespace Bakery.Tests
   [TestClass]
   public class PastryTests
   {
-     [TestMethod]
+    [TestMethod]
     public void TallyPastryOrder_Charges2DollarsForEachPastry_True()
     {
       Pastry testPastry = new Pastry();
       Assert.AreEqual(2, testPastry.TallyPastryOrder(1));
+    }
+    [TestMethod]
+    public void TallyPastryOrder_Charges1DollarsForEveryThirdPastry_True()
+    {
+      Pastry testPastry = new Pastry();
+      Assert.AreEqual(15, testPastry.TallyPastryOrder(9));
     }
   }
 }
