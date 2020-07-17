78777eae5b4308316d6209e2b78227af7aa2f653
Megan Hepner <megan.hepner@gmail.com>
2020-07-17 09:33:27 -0700

    run passing test TallyOrder_Charges5DollarsPerLoaf_True()

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -5,6 +5,16 @@ namespace Bakery
 {
   public class Bread
   {
+    public int BreadOrder { get; set; }
+    public int TallyOrder(int breadOrder)
+    {
+      int currentOrder = 0;
+      for (int i = 1; i <= breadOrder; i++)
+      {
+        currentOrder += 5;
+      }
+      return currentOrder;
+    }
 
     // properties, methods, etc. will go here.
 
